9a70990386848c1682558dcf93274ddbb2da4529
liyang <guzhoudiaoxue0706@163.com>
2024-10-25 13:36:03 +0800

    new

--- main.js
+++ main.js
@@ -746,7 +746,7 @@ async function main() {
     const url = new URL(
         // "nike.splat",
         // location.href,
-        params.get("url") || "output.splat",
+        params.get("url") || "output1.splat",
         // "https://huggingface.co/cakewalk/splat-data/resolve/main/",
         "https://coldsakrua.github.io/spikegs/output.splat",
     );
